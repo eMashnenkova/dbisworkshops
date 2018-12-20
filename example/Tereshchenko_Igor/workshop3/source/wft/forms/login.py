93b1615f36ba3ae88d73724c4308ed3c068d18d0
igortereshchenko <tereshchenko.igor@gmail.com>
2018-12-20 08:09:39 +0200

    w6

--- example/Tereshchenko_Igor/workshop3/source/wft/forms/login.py
+++ example/Tereshchenko_Igor/workshop3/source/wft/forms/login.py
@@ -1,13 +1,15 @@
 from flask_wtf import Form
-from wtforms import TextField, StringField, IntegerField, TextAreaField, SubmitField, RadioField, SelectField
+from wtforms import TextField, StringField, IntegerField, TextAreaField, SubmitField, RadioField, SelectField, DateField,PasswordField
 from flask import Flask, render_template, request, flash
 from wtforms import validators, ValidationError
 
 
 class LoginForm(Form):
 
-   email = StringField("Email",[ validators.DataRequired("Please enter your email address."),  validators.Email("Please enter your email address.")])
 
-   password = StringField("Password Of Student", [validators.DataRequired("Please enter your login.")])
+   studybook = StringField("Study Book: ",[ validators.DataRequired("Please enter your study book.")])
+   study_year = DateField("Study year: ", [ validators.DataRequired("Please enter your study year.")])
+
+   password = PasswordField("Password Of Student", [validators.DataRequired("Please enter your password.")])
 
    submit = SubmitField("Send")
